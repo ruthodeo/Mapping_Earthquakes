6763a8b8419c1aaa059b405a956933c74a71cb2f
Ruth OdeO Garcia <ortizdeoruegre@gmail.com>
2021-07-15 23:46:36 -0500

    first commit

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,38 @@
+// // Add console.log to check to see if our code is working.
+// console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yelow"
+}).addTo(map);
+
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//     color: 'blue',
+//     fillColor: '#ffffa1',
+//     // fillOpacity: 0.7,
+//     radius: 300
+// }).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// // Then we add the 'greymap' tile layer to the map.
+// satellite-streets-v11.addTo(map);
