adb4c53554643dd00c9ae316ff3c228773d5d60a
Ruth OdeO Garcia <ortizdeoruegre@gmail.com>
2021-07-16 18:39:26 -0500

    first commit

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -6,15 +6,35 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     maxZoom: 18,
     accessToken: API_KEY
 });
-// // Then we add the 'greymap' tile layer to the map.
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add the 'greymap' tile layer to the map.
 streets.addTo(map);
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/ruthodeo/Mapping_Earthquakes/main/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/ruthodeo/Mapping_Earthquakes/main/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
-});
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data),{
+  color:"#ffffa1",
+  weight: 2,
+  onEachFeature:function(feature, layer) {
+    layer.bindPopup("<h3> Airline:" + feature.properties.airline + "</h3><hr><h3> Destination:"
+    +feature.properties.dst + "</h3>");
+  }
+}
 
+.addTo(map)
+});
